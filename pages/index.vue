87667c682c470441a83ecbccaed4b3cabea3c6f4
Nicolas Walcker <walcker_@outlook.com>
2024-09-07 11:02:57 -0300

    chore/deps: adds tailwind, typed router and eslint

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const today = ref(new Date())
+
+onMounted(() => {
+  setInterval(() => {
+    today.value = new Date()
+  }, 1000)
+})
+</script>
+
+<template>
+  <div class="flex size-full items-center justify-center bg-red-100 text-center">
+    <h1 class="text-5xl font-bold">
+      App Start
+      <p class="mt-5">
+        {{ today.toLocaleString() }}
+      </p>
+    </h1>
+  </div>
+</template>
