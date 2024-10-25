ea1061b1dae61695993fec1d80e738ebe30d84a6
Felipe Tolentino <felipe-pereira1@live.com>
2024-10-25 15:28:41 -0300

    improved sponsors section
    added links to sponsors sites
    increased logos opacity
    fixed Lume Robotics logo

--- components/Sponsors/Sponsors.vue
+++ components/Sponsors/Sponsors.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="mt-32 flex flex-col items-center gap-6">
-    <p class="lg:hidden">
-      APOIO
+    <p class="mb-10 text-lg text-primary md:text-xl">
+      CONHEÇA NOSSOS PATROCINADORES
     </p>
-    <div class="flex flex-wrap items-center justify-center gap-20 opacity-15 grayscale md:flex-row lg:px-80">
-      <img src="/sponsors/adapti.png" class="h-auto w-32">
-      <img src="/sponsors/allwork.png" class="h-auto w-32">
-      <img src="/sponsors/highquality.png" class="h-auto w-48">
-      <img src="/sponsors/kairos.svg" class="h-auto w-24">
-      <img src="/sponsors/lume.png" class="h-auto w-32">
-      <img src="/sponsors/sxtech.png" class="h-auto w-32">
-      <img src="/sponsors/vestebem.png" class="h-auto w-32">
-      <img src="/sponsors/tribuna-do-cricare.png" class="h-auto w-32">
-      <img src="/sponsors/universidade-paralela.png" class="h-auto w-32">
+    <div class="flex flex-wrap items-center justify-center gap-20 opacity-30 grayscale md:flex-row lg:px-80">
+      <NuxtLink to="https://www.adapti.info/" external target="_blank">
+        <img src="/sponsors/adapti.png" class="h-auto w-32" title="Adapti - Soluções Web">
+      </NuxtLink>
+
+      <NuxtLink to="https://allworkservicos.com/" external target="_blank">
+        <img src="/sponsors/allwork.png" class="h-auto w-32" title="All Work Serviços">
+      </NuxtLink>
+
+      <NuxtLink to="https://www.hqcomunicacaovisual.com.br/" external target="_blank">
+        <img src="/sponsors/highquality.png" class="h-auto w-48" title="High Quality - Comunicação Visual">
+      </NuxtLink>
+
+      <NuxtLink to="https://www.radiokairos.com.br/" external target="_blank">
+        <img src="/sponsors/kairos.svg" class="h-auto w-24" title="Rádio Kairos - A Rádio da Família">
+      </NuxtLink>
+
+      <NuxtLink to="https://www.lumerobotics.ai/" external target="_blank">
+        <img src="/sponsors/lume.png" class="h-auto w-32" title="Lume Robotics - Sistema Lume de Mobilidade Autônoma">
+      </NuxtLink>
+
+      <NuxtLink to="https://www.instagram.com/sxtech.softwares/" external target="_blank">
+        <img src="/sponsors/sxtech.png" class="h-auto w-32" title="SX Tech">
+      </NuxtLink>
+
+      <NuxtLink to="https://vestbem.com/" external target="_blank">
+        <img src="/sponsors/vestebem.png" class="h-auto w-32" title="Vest Bem Confecções">
+      </NuxtLink>
+
+      <NuxtLink to="https://tconline.com.br/" external target="_blank">
+        <img src="/sponsors/tribuna-do-cricare.png" class="h-auto w-32" title="Tribuna do Cricaré">
+      </NuxtLink>
+
+      <NuxtLink to="https://universidadeparalela.com.br/" external target="_blank">
+        <img src="/sponsors/universidade-paralela.png" class="h-auto w-32" title="Universidade Paralela">
+      </NuxtLink>
     </div>
   </div>
 </template>
