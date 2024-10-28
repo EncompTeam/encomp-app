59c41f50af734cb0513c5def7bc32bdb20abedac
Felipe Tolentino <felipe-pereira1@live.com>
2024-10-28 18:53:59 -0300

    herick lima photo

--- components/Speakers/Speakers.vue
+++ components/Speakers/Speakers.vue
@@ -58,7 +58,7 @@ import { MicVocal } from 'lucide-vue-next'
       />
 
       <SpeakersCard
-        image="/speakers/questionmark.png"
+        image="/speakers/herick_lima.png"
         name="Herick Lima"
         role="Membro do Núcleo Espírito Santense de Computação Musical (NESCoM)"
         description="Herick Lima tem 28 anos e é aluno do curso de Ciência da Computação. Além de pesquisador no Núcleo Espírito-Santense de Computação Musical, trabalha como desenvolvedor Android na UpCities Govtech. Tem interesse nas áreas de computação musical, programação criativa e sistemas embarcados."
